8bb721e2fa3cf1d8e2c525784d38c3803e82c9ed
Hiendeptrai <kstzoro00001@gmail.com>
2025-12-27 20:20:49 +0700

    ok k

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Flutter',
+      home: const HelloScreen(),
+    );
+  }
+}
+
+class HelloScreen extends StatelessWidget {
+  const HelloScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello Flutter 👋',
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
